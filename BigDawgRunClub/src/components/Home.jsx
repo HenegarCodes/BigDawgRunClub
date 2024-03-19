a350d9c89687749ae81de4b3510068a78a15f8ce
spencerhenegar-Zwufst <spencerhenegar@gmail.com>
2024-03-18 20:08:53 -0700

    additions and subtractions to homepagez

--- BigDawgRunClub/src/components/Home.jsx
+++ BigDawgRunClub/src/components/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import './home.css'
 const Home = () => {
   return (
     <div className="home-page">
-      <h1>Welcome to Big Dawg Run Club</h1>
+      <h1 id='title'>Big Dawg Run Club</h1>
       <p>Explore our running events, connect with other runners, and share your photos!</p>
+      <div>
+      <p>How do I get involved?</p>
+        <ul>
+          <li>Click the Navigation bar</li>
+          <li>Select Get Notified</li>
+          <li>Fill out the form</li>
+          <li>Click submit and you will eb notified on updates and can submit photos to our team for review for our photo gallery!</li>
+          </ul>
     </div>
+    </div>
+
   );
 };
 
